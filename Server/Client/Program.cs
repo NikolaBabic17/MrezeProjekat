a347adf93bd1b93e8cd83dbe709397253a5bb48c
Dobi04 <nj675896@gmail.com>
2026-01-21 21:21:22 +0100

    Potpis Nikola J PR3/2023

--- Server/Client/Program.cs
+++ Server/Client/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Nikola Jovanovic PR3/2023
+
 namespace Client
 {
     internal static class Program
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Nikola Jovanovic PR3/2023
+
 namespace Server
 {
     internal static class Program
