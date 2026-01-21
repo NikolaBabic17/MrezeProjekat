cec796c5e68846dd2a7019166b3b7cdf7f8bc381
NikolaBabic17 <145857817+NikolaBabic17@users.noreply.github.com>
2026-01-21 21:04:36 +0100

    Project setup

---
+++ Server/Server/NewMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class NewMain
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello, Server!");
+            int karakter = Console.Read();
+            Console.WriteLine($"{karakter} - {karakter}");
+        }
+    }
+}
